18d1a6af1d864ac5939d6d10baf1fe454855f2e4
zhangqiang012 <542886873@qq.com>
2024-01-02 11:36:14 +0800

    add innotech device and lcd service

---
+++ examples/get-started/sample_project/main/innotech/lcd/innotech_lcd.c
@@ -0,0 +1,31 @@
+/******************** (C) COPYRIGHT 2021 INNOTECH **************************
+* COMPANY:			INNOTECH
+* DATE:				2023/12
+* AUTHOR:			qiang.zhang
+* IC:				ESP32S3
+* DESCRIPTION:	    Lcd Control.
+*____________________________________________________________________________
+* REVISION  Date		    User            Description
+* 1.0		2023/12/20	    qiang.zhang		First release
+*
+*____________________________________________________________________________
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "api_bridge.h"
+#include "innotech_lcd.h"
+
+void innotech_lcd_process(void)
+{ 
+
+}
+
+void innotech_lcd_init(void)
+{
+    
+}
+
--- examples/get-started/sample_project/main/main.c
+++ examples/get-started/sample_project/main/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include "innotech_device.h"
 
 void app_main(void)
 {
-
+    innotech_device_init();
 }
