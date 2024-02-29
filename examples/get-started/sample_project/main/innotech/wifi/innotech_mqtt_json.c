7278c26f0f0ef1547e62929e5f31382068240f8e
zhangqiang012 <542886873@qq.com>
2024-02-29 19:11:52 +0800

    add device timer and sleep mode mqtt json data pack and unpack

--- examples/get-started/sample_project/main/innotech/wifi/innotech_mqtt_json.c
+++ examples/get-started/sample_project/main/innotech/wifi/innotech_mqtt_json.c
@@ -19,106 +19,26 @@
 #include "innotech_config.h"
 #include "innotech_relay.h"
 
-#if 0
-//timer
-static void mqtt_json_pack_timer(char *cmd, char *package_msg)
+static void mqtt_json_pack_sleep(cJSON *sleepObject, uint8_t id)
 {
-    cJSON *timer1 = NULL, *timer2 = NULL, *timer3 = NULL, *timer4 = NULL, *timer5 = NULL;
-
-    if( (strncmp(cmd, "LocalTimer_1", strlen(cmd)) == 0) || (strncmp(cmd, "status", strlen(cmd)) == 0) )
-    {
-        timer1 = cJSON_CreateObject();
-        cJSON_AddStringToObject(timer1, "ScheduleId", innotech_config->timer[0].schedule_id);
-        cJSON_AddNumberToObject(timer1, "Enable", innotech_config->timer[0].enable);
-        cJSON_AddNumberToObject(timer1, "IsValid", innotech_config->timer[0].is_valid);
-        cJSON_AddStringToObject(timer1, "Timer",innotech_config->timer[0].time);
-        cJSON_AddNumberToObject(timer1, "PowerSwitch", innotech_config->timer[0].onoff);
-        cJSON_AddStringToObject(timer1, "Repeat", innotech_config->timer[0].repeat);
-        cJSON_AddItemToObject(stateJSObject, "LocalTimer_1", timer1);
-    }        
-    if( (strncmp(cmd, "LocalTimer_2", strlen(cmd)) == 0) || (strncmp(cmd, "status", strlen(cmd)) == 0) )
-    {
-        timer2 = cJSON_CreateObject();
-        cJSON_AddStringToObject(timer2, "ScheduleId", innotech_config->timer[1].schedule_id);
-        cJSON_AddNumberToObject(timer2, "Enable", innotech_config->timer[1].enable);
-        cJSON_AddNumberToObject(timer2, "IsValid", innotech_config->timer[1].is_valid);
-        cJSON_AddStringToObject(timer2, "Timer",innotech_config->timer[1].time);
-        cJSON_AddNumberToObject(timer2, "PowerSwitch", innotech_config->timer[1].onoff);
-        cJSON_AddStringToObject(timer2, "Repeat", innotech_config->timer[1].repeat);
-        cJSON_AddItemToObject(stateJSObject, "LocalTimer_2", timer2);
-    } 
-    if( (strncmp(cmd, "LocalTimer_3", strlen(cmd)) == 0) || (strncmp(cmd, "status", strlen(cmd)) == 0) )
-    {
-        timer3 = cJSON_CreateObject();
-        cJSON_AddStringToObject(timer3, "ScheduleId", innotech_config->timer[2].schedule_id);
-        cJSON_AddNumberToObject(timer3, "Enable", innotech_config->timer[2].enable);
-        cJSON_AddNumberToObject(timer3, "IsValid", innotech_config->timer[2].is_valid);
-        cJSON_AddStringToObject(timer3, "Timer",innotech_config->timer[2].time);
-        cJSON_AddNumberToObject(timer3, "PowerSwitch", innotech_config->timer[2].onoff);
-        cJSON_AddStringToObject(timer3, "Repeat", innotech_config->timer[2].repeat);
-        cJSON_AddItemToObject(stateJSObject, "LocalTimer_3", timer3);
-    }
-    if( (strncmp(cmd, "LocalTimer_4", strlen(cmd)) == 0) || (strncmp(cmd, "status", strlen(cmd)) == 0) )
-    {
-        timer4 = cJSON_CreateObject();
-        cJSON_AddStringToObject(timer4, "ScheduleId", innotech_config->timer[3].schedule_id);
-        cJSON_AddNumberToObject(timer4, "Enable", innotech_config->timer[3].enable);
-        cJSON_AddNumberToObject(timer4, "IsValid", innotech_config->timer[3].is_valid);
-        cJSON_AddStringToObject(timer4, "Timer",innotech_config->timer[3].time);
-        cJSON_AddNumberToObject(timer4, "PowerSwitch", innotech_config->timer[3].onoff);
-        cJSON_AddStringToObject(timer4, "Repeat", innotech_config->timer[3].repeat);
-        cJSON_AddItemToObject(stateJSObject, "LocalTimer_4", timer4);
-    }
-    if( (strncmp(cmd, "LocalTimer_5", strlen(cmd)) == 0) || (strncmp(cmd, "status", strlen(cmd)) == 0) )
-    {
-        timer5 = cJSON_CreateObject();
-        cJSON_AddStringToObject(timer5, "ScheduleId", innotech_config->timer[4].schedule_id);
-        cJSON_AddNumberToObject(timer5, "Enable", innotech_config->timer[4].enable);
-        cJSON_AddNumberToObject(timer5, "IsValid", innotech_config->timer[4].is_valid);
-        cJSON_AddStringToObject(timer5, "Timer",innotech_config->timer[4].time);
-        cJSON_AddNumberToObject(timer5, "PowerSwitch", innotech_config->timer[4].onoff);
-        cJSON_AddStringToObject(timer5, "Repeat", innotech_config->timer[4].repeat);
-        cJSON_AddItemToObject(stateJSObject, "LocalTimer_5", timer5);
-    }
+    innotech_config_t *innotech_config = (innotech_config_t *)innotech_config_get_handle();
+    cJSON_AddItemToObject(sleepObject, "PowerSwitch", cJSON_CreateNumber(innotech_config->sleep[id].onoff));			 
+    cJSON_AddItemToObject(sleepObject, "TimeLeft", cJSON_CreateNumber(innotech_config->sleep[id].time_left));   
+    cJSON_AddItemToObject(sleepObject, "IsRunning", cJSON_CreateNumber(innotech_config->sleep[id].is_running));
+    cJSON_AddItemToObject(sleepObject, "ScheduleId", cJSON_CreateString((const char *)innotech_config->sleep[id].schedule_id));
+    cJSON_AddItemToObject(sleepObject, "Timestamp", cJSON_CreateString((const char *)innotech_config->sleep[id].timestamp));
 }
 
-//delayClose
-static void mqtt_json_pack_count_down(char *cmd, char *package_msg)
+static void mqtt_json_pack_timer(cJSON *timerObject, uint8_t id)
 {
-    cJSON *count_down1 = NULL, *count_down2 = NULL, *count_down3 = NULL;
-
-    if( (strncmp(cmd, "CountDown_1", strlen(cmd)) == 0) || (strncmp(cmd, "status", strlen(cmd)) == 0) )
-    {
-        count_down1 = cJSON_CreateObject();
-        cJSON_AddStringToObject(count_down1, "ScheduleId", innotech_config->sleep[0].schedule_id);
-        cJSON_AddNumberToObject(count_down1, "TimeLeft", innotech_config->sleep[0].time_left);
-        cJSON_AddNumberToObject(count_down1, "PowerSwitch", innotech_config->sleep[0].onoff);
-        cJSON_AddStringToObject(count_down1, "Timestamp",innotech_config->sleep[0].timestamp);
-        cJSON_AddNumberToObject(count_down1, "IsRunning", innotech_config->sleep[0].is_running);
-        cJSON_AddItemToObject(stateJSObject, "CountDown_1", count_down1);
-    }
-    if( (strncmp(cmd, "CountDown_2", strlen(cmd)) == 0) || (strncmp(cmd, "status", strlen(cmd)) == 0) )
-    {
-        count_down2 = cJSON_CreateObject();
-        cJSON_AddStringToObject(count_down2, "ScheduleId", innotech_config->sleep[1].schedule_id);
-        cJSON_AddNumberToObject(count_down2, "TimeLeft", innotech_config->sleep[1].time_left);
-        cJSON_AddNumberToObject(count_down2, "PowerSwitch", innotech_config->sleep[1].onoff);
-        cJSON_AddStringToObject(count_down2, "Timestamp",innotech_config->sleep[1].timestamp);
-        cJSON_AddNumberToObject(count_down2, "IsRunning", innotech_config->sleep[1].is_running);
-        cJSON_AddItemToObject(stateJSObject, "CountDown_2", count_down2);
-    }
-    if( (strncmp(cmd, "CountDown_3", strlen(cmd)) == 0) || (strncmp(cmd, "status", strlen(cmd)) == 0) )
-    {
-        count_down3 = cJSON_CreateObject();
-        cJSON_AddStringToObject(count_down3, "ScheduleId", innotech_config->sleep[2].schedule_id);
-        cJSON_AddNumberToObject(count_down3, "TimeLeft", innotech_config->sleep[2].time_left);
-        cJSON_AddNumberToObject(count_down3, "PowerSwitch", innotech_config->sleep[2].onoff);
-        cJSON_AddStringToObject(count_down3, "Timestamp",innotech_config->sleep[2].timestamp);
-        cJSON_AddNumberToObject(count_down3, "IsRunning", innotech_config->sleep[2].is_running);
-        cJSON_AddItemToObject(stateJSObject, "CountDown_3", count_down3);
-    }  
+    innotech_config_t *innotech_config = (innotech_config_t *)innotech_config_get_handle();
+    cJSON_AddItemToObject(timerObject, "PowerSwitch", cJSON_CreateNumber(innotech_config->timer[id].onoff));			 
+    cJSON_AddItemToObject(timerObject, "Timer", cJSON_CreateString((const char *)innotech_config->timer[id].time));   
+    cJSON_AddItemToObject(timerObject, "Enable", cJSON_CreateNumber(innotech_config->timer[id].enable));
+    cJSON_AddItemToObject(timerObject, "ScheduleId", cJSON_CreateString((const char *)innotech_config->timer[id].schedule_id));
+    cJSON_AddItemToObject(timerObject, "IsValid", cJSON_CreateNumber(innotech_config->timer[id].is_valid));
+    cJSON_AddItemToObject(timerObject, "Repeat", cJSON_CreateString((const char *)innotech_config->timer[id].repeat));
 }
-#endif
 
 int mqtt_json_last_will(char *result, char * timestamp, char *package_msg)
 {
@@ -153,6 +73,7 @@ int mqtt_json_last_will(char *result, char * timestamp, char *package_msg)
 
 static int mqtt_json_params_pack(char *cmd, cJSON *stateJSObject)
 {
+    cJSON *timeJSObject  = NULL;
     innotech_config_t *innotech_config = (innotech_config_t *)innotech_config_get_handle();
 
     if( (strncmp(cmd, "PowerSwitch", strlen(cmd)) == 0) || (strncmp(cmd, "status", strlen(cmd)) == 0))
@@ -209,6 +130,48 @@ static int mqtt_json_params_pack(char *cmd, cJSON *stateJSObject)
     {
         cJSON_AddItemToObject(stateJSObject, "LineDiameter", cJSON_CreateNumber(0.0));
     }
+
+    if( (strncmp(cmd, "LocalTimer_1", strlen(cmd)) == 0))
+    {
+        cJSON_AddItemToObject(stateJSObject, "LocalTimer_1", timeJSObject = cJSON_CreateObject());
+        mqtt_json_pack_timer(timeJSObject, 0);
+    }
+    else if( (strncmp(cmd, "LocalTimer_2", strlen(cmd)) == 0))
+    {
+        cJSON_AddItemToObject(stateJSObject, "LocalTimer_2", timeJSObject = cJSON_CreateObject());
+        mqtt_json_pack_timer(timeJSObject, 1);
+    }
+    else if( (strncmp(cmd, "LocalTimer_3", strlen(cmd)) == 0))
+    {
+        cJSON_AddItemToObject(stateJSObject, "LocalTimer_3", timeJSObject = cJSON_CreateObject());
+        mqtt_json_pack_timer(timeJSObject, 2);
+    }
+    else if( (strncmp(cmd, "LocalTimer_4", strlen(cmd)) == 0))
+    {
+        cJSON_AddItemToObject(stateJSObject, "LocalTimer_4", timeJSObject = cJSON_CreateObject());
+        mqtt_json_pack_timer(timeJSObject, 3);
+    }
+    else if( (strncmp(cmd, "LocalTimer_5", strlen(cmd)) == 0))
+    {
+        cJSON_AddItemToObject(stateJSObject, "LocalTimer_5", timeJSObject = cJSON_CreateObject());
+        mqtt_json_pack_timer(timeJSObject, 4);
+    }
+    else if( (strncmp(cmd, "CountDown_1", strlen(cmd)) == 0))
+    {
+        cJSON_AddItemToObject(stateJSObject, "CountDown_1", timeJSObject = cJSON_CreateObject());
+        mqtt_json_pack_sleep(timeJSObject, 0);
+    }
+    else if( (strncmp(cmd, "CountDown_2", strlen(cmd)) == 0))
+    {
+        cJSON_AddItemToObject(stateJSObject, "CountDown_2", timeJSObject = cJSON_CreateObject());
+        mqtt_json_pack_sleep(timeJSObject, 1);
+    }
+    else if( (strncmp(cmd, "CountDown_3", strlen(cmd)) == 0))
+    {
+        cJSON_AddItemToObject(stateJSObject, "CountDown_3", timeJSObject = cJSON_CreateObject());
+        mqtt_json_pack_sleep(timeJSObject, 2);
+    }
+    
     return 0;
 }
 
@@ -234,7 +197,15 @@ int mqtt_json_pack(char *cmd, char *id, char *version, char *package_msg)
             (strncmp(cmd, "ScreenSwitch", strlen(cmd)) == 0) ||\
             (strncmp(cmd, "AutoBrightnessSwitch", strlen(cmd)) == 0) ||\
             (strncmp(cmd, "Memory", strlen(cmd)) == 0) ||\
-			(strncmp(cmd, "LineDiameter", strlen(cmd)) == 0))
+            (strncmp(cmd, "LineDiameter", strlen(cmd)) == 0) ||\
+            (strncmp(cmd, "LocalTimer_1", strlen(cmd)) == 0) ||\
+            (strncmp(cmd, "LocalTimer_2", strlen(cmd)) == 0) ||\
+            (strncmp(cmd, "LocalTimer_3", strlen(cmd)) == 0) ||\
+            (strncmp(cmd, "LocalTimer_4", strlen(cmd)) == 0) ||\
+            (strncmp(cmd, "LocalTimer_5", strlen(cmd)) == 0) ||\
+            (strncmp(cmd, "CountDown_1", strlen(cmd)) == 0) ||\
+            (strncmp(cmd, "CountDown_2", strlen(cmd)) == 0) ||\
+			(strncmp(cmd, "CountDown_3", strlen(cmd)) == 0))
 		{
 			cJSON_AddItemToObject(IOTJSObject, "params", stateJSObject = cJSON_CreateObject());
             cJSON_AddItemToObject(IOTJSObject, "sys", sysJSObject = cJSON_CreateObject());
@@ -281,6 +252,74 @@ void mqtt_json_pack_reply(char *id, char *version, char *package_msg)
 
 }
 
+static void mqtt_json_unpack_sleep(cJSON *sleepObject, uint8_t id)
+{
+    if(sleepObject == NULL)
+    {
+        printf("unpack_params null\n");
+        return;
+    }
+    cJSON *onoffObject, *timeObject, *runObject, *stampObject, *idObject;
+    innotech_config_t *innotech_config = (innotech_config_t *)innotech_config_get_handle();
+
+    if( (onoffObject = cJSON_GetObjectItem(sleepObject, "PowerSwitch")) != NULL )
+    {
+        innotech_config->sleep[id].onoff = onoffObject->valueint;
+    }
+    if( (timeObject = cJSON_GetObjectItem(sleepObject, "TimeLeft")) != NULL )
+    {
+        innotech_config->sleep[id].time_left = timeObject->valueint;
+    }
+    if( (runObject = cJSON_GetObjectItem(sleepObject, "IsRunning")) != NULL )
+    {
+        innotech_config->sleep[id].is_running = runObject->valueint;
+    }
+    if( (idObject = cJSON_GetObjectItem(sleepObject, "ScheduleId")) != NULL )
+    {
+        memcpy((void *)innotech_config->sleep[id].schedule_id, (void *)idObject->valuestring, strlen(idObject->valuestring)+1);
+    }
+    if( (stampObject = cJSON_GetObjectItem(sleepObject, "Timestamp")) != NULL )
+    {
+        memcpy((void *)innotech_config->sleep[id].timestamp, (void *)stampObject->valuestring, strlen(stampObject->valuestring)+1);
+    }
+}
+
+static void mqtt_json_unpack_timer(cJSON *timerObject, uint8_t id)
+{
+    if(timerObject == NULL)
+    {
+        printf("unpack_params null\n");
+        return;
+    }
+    cJSON *onoffObject, *timeObject, *enableObject, *repeatObject, *idObject, *validObject;
+    innotech_config_t *innotech_config = (innotech_config_t *)innotech_config_get_handle();
+
+    if( (onoffObject = cJSON_GetObjectItem(timerObject, "PowerSwitch")) != NULL )
+    {
+        innotech_config->timer[id].onoff = onoffObject->valueint;
+    }
+    if( (timeObject = cJSON_GetObjectItem(timerObject, "Timer")) != NULL )
+    {
+        memcpy((void *)innotech_config->timer[id].time, (void *)timeObject->valuestring, strlen(timeObject->valuestring)+1);
+    }
+    if( (enableObject = cJSON_GetObjectItem(timerObject, "Enable")) != NULL )
+    {
+        innotech_config->timer[id].enable = enableObject->valueint;
+    }
+    if( (repeatObject = cJSON_GetObjectItem(timerObject, "Repeat")) != NULL )
+    {
+        memcpy((void *)innotech_config->timer[id].repeat, (void *)repeatObject->valuestring, strlen(repeatObject->valuestring)+1);
+    }
+    if( (idObject = cJSON_GetObjectItem(timerObject, "ScheduleId")) != NULL )
+    {
+        memcpy((void *)innotech_config->timer[id].schedule_id, (void *)idObject->valuestring, strlen(idObject->valuestring)+1);
+    }
+    if( (validObject = cJSON_GetObjectItem(timerObject, "IsValid")) != NULL )
+    {
+        innotech_config->timer[id].is_valid = validObject->valueint;
+    }
+}
+
 static void mqtt_json_unpack_params(cJSON *paramsObject, char *get_cmd)
 {
     if(paramsObject == NULL)
@@ -321,6 +360,46 @@ static void mqtt_json_unpack_params(cJSON *paramsObject, char *get_cmd)
     {
         innotech_config->memory = data_Object->valueint;
         memcpy(get_cmd, "Memory", strlen("Memory")+1);
+    }
+    else if( (data_Object = cJSON_GetObjectItem(paramsObject, "LocalTimer_1")) != NULL )
+    {
+        mqtt_json_unpack_timer(data_Object, 0);
+        memcpy(get_cmd, "LocalTimer_1", strlen("LocalTimer_1")+1);
+    }
+    else if( (data_Object = cJSON_GetObjectItem(paramsObject, "LocalTimer_2")) != NULL )
+    {
+        mqtt_json_unpack_timer(data_Object, 1);
+        memcpy(get_cmd, "LocalTimer_2", strlen("LocalTimer_2")+1);
+    }
+    else if( (data_Object = cJSON_GetObjectItem(paramsObject, "LocalTimer_3")) != NULL )
+    {
+        mqtt_json_unpack_timer(data_Object, 2);
+        memcpy(get_cmd, "LocalTimer_3", strlen("LocalTimer_3")+1);
+    }
+    else if( (data_Object = cJSON_GetObjectItem(paramsObject, "LocalTimer_4")) != NULL )
+    {
+        mqtt_json_unpack_timer(data_Object, 3);
+        memcpy(get_cmd, "LocalTimer_4", strlen("LocalTimer_4")+1);
+    }
+    else if( (data_Object = cJSON_GetObjectItem(paramsObject, "LocalTimer_5")) != NULL )
+    {
+        mqtt_json_unpack_timer(data_Object, 4);
+        memcpy(get_cmd, "LocalTimer_5", strlen("LocalTimer_5")+1);
+    }
+    else if( (data_Object = cJSON_GetObjectItem(paramsObject, "CountDown_1")) != NULL )
+    {
+        mqtt_json_unpack_sleep(data_Object, 0);
+        memcpy(get_cmd, "CountDown_1", strlen("CountDown_1")+1);
+    }
+    else if( (data_Object = cJSON_GetObjectItem(paramsObject, "CountDown_2")) != NULL )
+    {
+        mqtt_json_unpack_sleep(data_Object, 1);
+        memcpy(get_cmd, "CountDown_2", strlen("CountDown_2")+1);
+    }
+    else if( (data_Object = cJSON_GetObjectItem(paramsObject, "CountDown_3")) != NULL )
+    {
+        mqtt_json_unpack_sleep(data_Object, 2);
+        memcpy(get_cmd, "CountDown_3", strlen("CountDown_3")+1);
     }
 }
 
