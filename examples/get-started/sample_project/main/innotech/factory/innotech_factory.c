b877d39f4f32373f56329139aed7a9f498297ad9
zhangqiang012 <542886873@qq.com>
2024-04-02 16:05:59 +0800

    add factory scan wifi function

--- examples/get-started/sample_project/main/innotech/factory/innotech_factory.c
+++ examples/get-started/sample_project/main/innotech/factory/innotech_factory.c
@@ -11,9 +11,19 @@
 *____________________________________________________________________________
 
 *****************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "innotech_wifi.h"
 #include "innotech_config.h"
 #include "innotech_factory.h"
+#include "innotech_relay.h"
+#include "innotech_button.h"
+#include "innotech_meter.h"
+
+
+#define FACTORY_SSID "gnpro*GB3Z*"
 
 extern void esp_restart(void);
 void innotech_factory_reset(void)
@@ -25,6 +35,28 @@ void innotech_factory_reset(void)
 
 void innotech_factory_init(void)
 {
+    bool factory_flag = false;
 
+    innotech_netif_init();
+
+    for(int i = 0; i < 2; i++)
+    {
+        if(innotech_wifi_scan((uint8_t*)FACTORY_SSID))
+        {
+            factory_flag = true;
+            innotech_relay_init();
+            innotech_button_init();
+            innotech_meter_init();
+            break;
+        }
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+
+    while(factory_flag)
+    {
+        innotech_button_process();
+        vTaskDelay(20 / portTICK_PERIOD_MS);
+    }
+    
 }
 
